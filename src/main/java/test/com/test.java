94b7c459cce5814a6577ffcaa4c1615409e1e138
happydia88 <happydia88@gmail.com>
2020-03-17 22:08:36 +0800

    test

---
+++ src/main/java/test/com/test.java
@@ -0,0 +1,17 @@
+package test.com;
+
+/**
+ * @author Jerry.Mu @create 2020-03-16 21:52
+ */
+public class test {
+
+    public static void main(String[] args) {
+//        System.out.println("hello java");
+//new person().hello();
+        person p=new person();
+        p.hello();
+    }
+
+}
+
+
